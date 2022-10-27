555a2fe94b771e7cee0bbaaaf56472279510c766
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2022-10-27 20:14:16 -0300

    Atualização exercício bom dia

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -6,10 +6,13 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
     img.src = 'manha.png'
+    document.body.style.backgroundColor = '#e2cd9f'
 } else if (hora >= 12 && hora < 18) {
     img.src = 'tarde.png'
+    document.body.style.backgroundColor = '#b9846f'
 } else {
     img.src = 'noite.png'
+    document.body.style.backgroundColor = '#515154'
 }
 
 }
