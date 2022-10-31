96c5d0d8b21bfa93a02eddacb0eaea7c29526356
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2022-10-31 13:53:42 -0300

    Atualização condições

--- Aula12ex/ex015/script.js
+++ Aula12ex/ex015/script.js
@@ -9,12 +9,33 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem_masculino.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'homem.png')
+            } else {
+                img.setAttribute('src', 'idoso.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem_feminino.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                img.setAttribute('src', 'idosa.png')
+            }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos gênero ${gênero} com ${idade} anos.`
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
