c36264dfe58e39551b264ada0b634be19ee03457
Gabriel <107438394+gabrielfinazzo@users.noreply.github.com>
2022-11-08 22:13:18 -0300

    Atualização vetores

---
+++ Aula 15/ambiente.js
@@ -0,0 +1,25 @@
+/*let num = [4,7,3,2,9,8]
+for(let pos = 0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+
+let num = [4,5,8,7,1,2,6,2]
+let pos = 0
+while (pos < num.length) {
+    pos++
+    console.log(num[pos])
+}
+*/
+/*
+let num = [9,1,6,2,7,3,4,8,7,9]
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+num.indexOf(7)
+*/
+
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
